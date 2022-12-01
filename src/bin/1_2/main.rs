98ba63ad698933aed994cd47d272134bd6b7f4fe
Matt Turner <matturner@gmail.com>
2022-12-01 18:20:10 +0000

    More functional

--- src/bin/1_2/main.rs
+++ src/bin/1_2/main.rs
@@ -1,4 +1,3 @@
-use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{self, BufRead};
 
@@ -7,19 +6,16 @@ fn main() -> anyhow::Result<()> {
     // Seems there's no .sorted()
     let mut elves = io::BufReader::new(f)
         .lines()
-        .fold(VecDeque::from([0]), |mut es, l| {
-            match l.expect("Failed to read line").as_ref() {
-                "" => {
-                    es.push_front(0);
-                }
-                a => {
-                    let n = a.parse::<i64>().unwrap();
-                    es[0] += n;
-                }
-            }
-            es
-        });
-    elves.make_contiguous().sort_unstable_by(|a, b| b.cmp(a));
+        .flatten() // Remove read errors
+        .collect::<Vec<String>>() // Iterator doesn't have split()
+        .split(|l| l == "")
+        .map(|ls| {
+            ls.iter()
+                .map(|l| l.parse::<i64>().expect("parse error"))
+                .sum()
+        })
+        .collect::<Vec<i64>>();
+    elves.sort_unstable_by(|a, b| b.cmp(a));
     let sum = elves.iter().take(3).sum::<i64>();
 
     println!("Greediest three elves have {} cals", sum);
